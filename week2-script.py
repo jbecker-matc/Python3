2d8a1aa08323bcb9d20b8bb1bbe11a329fed56f0
root <root@kali-python.seclan.local>
2021-01-30 19:30:42 -0600

    READY FOR GRADING

---
+++ week2-script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+prints "Jon Becker" to the screen
+prints one thing I learned about python to the screen
+Usage: ./week2-script.py
+"""
+
+print ("Jon becker")
+
+print ("I learned that a \# causes python to only consider anything afterwards as a script comment")
+
